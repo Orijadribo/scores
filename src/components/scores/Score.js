14d502342cd008b407c2a0993242b994201483b5
Daniel Orija <danielorijadribo@gmail.com>
2024-01-28 01:39:37 +0300

    Center

--- src/components/scores/Score.js
+++ src/components/scores/Score.js
@@ -111,21 +111,21 @@ const Score = () => {
         <div className="flex items-center justify-center gap-4 pt-5">
           <div className="flex items-center justify-center gap-4">
             <div className="flex items-center justify-center w-5 h-5 border border-black rounded-full">
-              <div className=" w-4 h-4 bg-black  rounded-full"></div>
+              <div className="flex items-center justify-center w-4 h-4 bg-black  rounded-full"></div>
             </div>
             <p>eagle or better</p>
           </div>
           <div className="flex items-center justify-center gap-4">
-            <div className="w-5 h-5 bg-black rounded-full"></div>
+            <div className="flex items-center justify-center w-5 h-5 bg-black rounded-full"></div>
             <p>birdie</p>
           </div>
           <div className="flex items-center justify-center gap-4">
-            <div className="w-5 h-5 bg-black"></div>
+            <div className="flex items-center justify-center w-5 h-5 bg-black"></div>
             <p>bogey</p>
           </div>
           <div className="flex items-center justify-center gap-4">
             <div className="flex items-center justify-center w-5 h-5 border border-black">
-              <div className=" w-4 h-4 bg-black"></div>
+              <div className="flex items-center justify-center w-4 h-4 bg-black"></div>
             </div>
             <p>double bogey or better</p>
           </div>
