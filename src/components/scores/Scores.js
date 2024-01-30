739b058583229bbcb138f8d31ed71c4bb7435d12
Daniel Orija <danielorijadribo@gmail.com>
2024-01-30 23:32:14 +0300

    Responsive

--- src/components/scores/Scores.js
+++ src/components/scores/Scores.js
@@ -6,8 +6,8 @@ const Scores = () => {
 
   return (
     <div className="flex flex-col gap-4 max-w-5xl m-auto p-10 border rounded-xl bg-white">
-      <div className="flex items-center justify-between pb-6">
-        <p className="text-[28px]">Leaderboard</p>
+      <div className="md:flex items-center justify-between pb-6">
+        <p className="text-[28px] pb-6 md:pb-0">Leaderboard</p>
         <div className="flex items-center justify-between border px-5 py-2 rounded-full">
           <input
             type="text"
