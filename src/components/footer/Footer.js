5e87c5ca41a8399b6d8f6a22a14c272ee8233eef
Daniel Orija <danielorijadribo@gmail.com>
2024-01-31 22:43:26 +0300

    Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { WNGC_logo } from "../../assets";
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="mt-10 bg-[#2C3F46]">
+      <div className="flex flex-col md:flex-row max-w-4xl justify-between m-auto text-[#808080] py-8 md:px-2 px-10 gap-4">
+        <div className="flex flex-col md:items-center justify-center cursor-pointer">
+          <img src={WNGC_logo} alt="Wesnile Golf Club Logo" className="w-16 " />
+          <p className="mt-2">WestNile Golf Club</p>
+        </div>
+        <div className="">
+          <h4 className="py-2 font-titleFont text-xl font-bold">Contact Us</h4>
+          <div className="text-sm">
+            <p>Wesnile Golf Club</p>
+            <p>1234 Fairway Lane</p>
+            <p>City, State 12345</p>
+            <p>Email: info@wesnilegolfclub.com</p>
+            <p>Phone: +256 789 012345</p>
+          </div>
+        </div>
+        <div className="footer-social">
+          <h4 className="py-2 font-titleFont text-xl font-bold">Follow Us</h4>
+          <ul className="text-sm">
+            <li className="flex items-center justify-start gap-4 cursor-pointer">
+              <FaFacebook />
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
+                Facebook
+              </a>
+            </li>
+            <li className="flex items-center justify-start gap-4 cursor-pointer">
+              <FaXTwitter />
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
+                Twitter
+              </a>
+            </li>
+            <li className="flex items-center justify-start gap-4 cursor-pointer">
+              <FaInstagram />
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="text-[#65787E] flex justify-center items-center w-full p-2 bg-[#1e2b30] text-sm">
+        <p>&copy; 2024 Wesnile Golf Club. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "../components/header/Header";
 import HeaderBottom from "../components/header/HeaderBottom";
 import Scores from "../components/scores/Scores";
+import Footer from "../components/footer/Footer";
 
 const Main = () => {
   return (
-    <div className="pb-20">
+    <div className="">
       <Header />
       <HeaderBottom />
       <Scores/>
+      <Footer/>
     </div>
   );
 };
